7592106fd700f590967cd2825f237af1f9926eef
vrb9999 <vbiradar9999@gmail.com>
2022-05-10 20:00:17 +0530

    UC- Day-14 Create Queue using linked list

---
+++ LinkedList_Queue/LinkedList_Queue/LinkedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList_Queue
+{
+    class LinkedList
+    {
+        Node head = null;
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+                head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into queue ", node.data);
+        }
+        public void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.Write(temp.data + "\n");
+            }
+        }
+    }
+}
---
+++ LinkedList_Queue/LinkedList_Queue/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinkedList_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Enqueue(56);
+            list.Enqueue(30);
+            list.Enqueue(70);
+            list.Display();
+        }
+    }
+}
