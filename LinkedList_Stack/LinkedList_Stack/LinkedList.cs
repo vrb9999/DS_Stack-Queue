7a72f007c8da17c09985a6168ae64f9ae79a40e9
vrb9999 <vbiradar9999@gmail.com>
2022-05-10 19:47:50 +0530

    UC-1 Day-14 Create stack using linked list

---
+++ LinkedList_Stack/LinkedList_Stack/LinkedList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList_Stack
+{
+    class LinkedList
+    {
+        private Node top;
+        public LinkedList()
+        {
+            this.top = null;
+        }
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ LinkedList_Stack/LinkedList_Stack/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinkedList_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Push(70);
+            list.Push(30);
+            list.Push(56);
+            list.Display();
+        }
+    }
+}
