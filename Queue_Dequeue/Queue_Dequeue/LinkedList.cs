9ad8adc85aaea6a04d224f4cd0256a0737d41938
vrb9999 <vbiradar9999@gmail.com>
2022-05-10 20:01:39 +0530

    UC-4 Day-14 Dequeue

---
+++ Queue_Dequeue/Queue_Dequeue/LinkedList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queue_Dequeue
+{
+    class LinkedList
+    {
+        Node head = null;
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+                head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into queue ", node.data);
+        }
+        public void Dequeue()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            else
+            {
+                Console.WriteLine(head.data + " removed from Queue");
+                head = head.next;
+            }
+        }
+        public void Isempty()
+        {
+            while(this.head != null)
+            {
+                Dequeue();
+            }
+        }
+        public void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.Write(temp.data + "\n");
+            }
+        }
+    }
+}
---
+++ Queue_Dequeue/Queue_Dequeue/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Queue_Dequeue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Enqueue(56);
+            list.Enqueue(30);
+            list.Enqueue(70);
+            list.Isempty();
+            list.Display();
+        }
+    }
+}
